ffcc3554841088a764f2652dba506692cd5e6d51
Luis Martinez <luis_mmartinez@live.com>
2021-01-07 06:56:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,11 @@ class Triangle
   end
 
   def kind
-    if (@a == @b && @b == @c) && (@a>0 && @b>0 && @c>0 && @a + @b > @c)
+    if (@a == @b && @b == @c) && (@a>0 && @b>0 && @c>0 && (@a + @b > @c))
       :equilateral
-    elsif (@a == @b || @a == @c || @b == @c) && (@a>0 && @b>0 && @c>0 && @a + @b > @c)
+    elsif (@a == @b || @a == @c || @b == @c) && (@a>0 && @b>0 && @c>0 && (@a + @b > @c))
       :isosceles
-    elsif (@a != @b && @b != @c) && (@a>0 && @b>0 && @c>0 && @a + @b > @c)
+    elsif (@a != @b && @b != @c) && (@a>0 && @b>0 && @c>0 && (@a + @b > @c))
 
 
 end
