a639cef3124532799da1f0348534d0360f8850bc
Luis Martinez <luis_mmartinez@live.com>
2021-01-07 07:27:49 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c, :sorted
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @sorted = [a, b, c].sort
+  end
+
+  def kind
+    if (@a>0 && @b>0 && @c>0) && (@sorted[0] + @sorted[1] > @sorted[2])
+      if (@a == @b && @b == @c)
+        :equilateral
+      elsif (@a == @b || @a == @c || @b == @c)
+        :isosceles
+      elsif (@a != @b && @b != @c)
+        :scalene
+      end
+    else
+      begin
+        raise TriangleError
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
