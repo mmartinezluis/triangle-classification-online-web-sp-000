7eab5c2d1f6da32f74752a3c9ba6f24dd5adc074
Luis Martinez <luis_mmartinez@live.com>
2021-01-07 06:54:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
   # write code here
   attr_accessor :a, :b, :case
-  
+
   def initialize(a, b, c)
     @a = a
     @b = b
@@ -9,6 +9,9 @@ class Triangle
   end
 
   def kind
+    if (@a == @b && @b == @c) && (@a>0 && @b>0 && @c>0 && @a + @b > @c)
+      :equilateral
+    elsif (@a == @b || @a == @c || @b == @c) && (@a>0 && @b>0 && @c>0 && @a + @b > @c)
 
 
 end
