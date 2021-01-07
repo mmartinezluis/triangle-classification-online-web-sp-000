6912c9c9fa1c7ae7a675b848d2f540380ddc90fc
Luis Martinez <luis_mmartinez@live.com>
2021-01-07 07:12:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
 
   def kind
-    if (@a>0 && @b>0 && @c>0) && (@a + @b > @c)
+    if (@a>0 && @b>0 && @c>0) && (@a + @b >= @c)
       if (@a == @b && @b == @c)
         :equilateral
       elsif (@a == @b || @a == @c || @b == @c)
